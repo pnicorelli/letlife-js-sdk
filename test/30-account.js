32d415be6ad52799415c72e53289f8970a7c4d9a
Paolo -Willy- Nicorelli <p.nicorelli@gmail.com>
2015-08-06 09:40:33 +0200

    test: 100% coverage until now

--- test/30-account.js
+++ test/30-account.js
@@ -14,28 +14,60 @@ describe('LetLife SDK - Account Services', function() {
 
   it('.emailExists() should throw an ApiError on failure', function(done){
     var scope = nock(config.apiUrl, {})
-        .get('/v1/email_exists/e.snowden@nsa.gov')
-        .reply(500, { });
+    .get('/v1/email_exists/e.snowden@nsa.gov')
+    .reply(500, { });
 
     account.emailExists('e.snowden@nsa.gov', function( err, result){
-        expect(err).to.be.an.instanceof(LF.ApiError);
-        err.statusCode.should.equal(500)
-        return done();
+      expect(err).to.be.an.instanceof(LF.ApiError);
+      err.statusCode.should.equal(500)
+      return done();
     });
   });
 
   it('.emailExists() should return a boolean on success', function(done){
     var scope = nock(config.apiUrl, {})
-        .get('/v1/email_exists/e.snowden@nsa.gov')
-        .reply(200, { result: true});
+    .get('/v1/email_exists/e.snowden@nsa.gov')
+    .reply(200, { result: true});
 
     account.emailExists('e.snowden@nsa.gov', function(err, res){
       expect(err).to.be.null;
       res.should.equal(true);
       return done();
-    })
+    });
 
   });
 
 
-})
+
+  it('.authToken() should throw an ApiError on failure', function(done){
+    var scope = nock(config.apiUrl, {})
+    .post('/auth/token')
+    .reply(500, {"grant_type": "password", "username": "myuser", "password": "mypassword"});
+
+    account.authToken("myuser", "mypassword", function(err, res){
+      expect(err).to.be.an.instanceof(LF.ApiError);
+      err.statusCode.should.equal(500)
+      return done();
+    });
+  });
+
+  it('.authToken() should return access_token on success', function(done){
+    var scope = nock(config.apiUrl, {})
+    .post('/auth/token', {"grant_type": "password", "username": "myuser", "password": "mypassword"})
+    .reply(200,
+      {
+        "access_token":"cv7i6rdfe7pblfo00lgnrb3cnj1mr8gcyrf72k6j",
+        "refresh_token":"f99fro3vrnoey0iytbw23k0ygvh78uq5p751spch",
+        "token_type":"Bearer"
+      });
+
+      account.authToken("myuser", "mypassword", function(err, res){
+        expect(err).to.be.null;
+        res.access_token.should.equal("cv7i6rdfe7pblfo00lgnrb3cnj1mr8gcyrf72k6j");
+        res.token_type.should.equal("Bearer");
+        return done();
+      });
+
+    });
+
+  })
